2c20685620b54bb0a0116cfee90e1a3a248dc06e
Christian Urcuqui <rhapsody_cris@hotmail.com>
2015-08-21 00:23:34 -0500

    Update TcpClient.py

--- TcpClient.py
+++ TcpClient.py
@@ -7,7 +7,8 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((target_host, target_port))
 
-client.send("GET / HTTP/1.1/r\nHost: google.com\r\n\r\n")
+#Message will send
+#client.send("GET / HTTP/1.1/r\nHost: google.com\r\n\r\n")
 
 response = client.recv(4096)
 
