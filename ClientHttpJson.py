4fcfa24f331ae36212aac569922a2c9ef2533915
Christian Urcuqui <ulcamilo@gmail.com>
2016-02-03 14:20:24 -0500

    server & client http
    server & client http

--- ClientHttpJson.py
+++ ClientHttpJson.py
@@ -1,44 +1,20 @@
 import http.client
 import json
 import socket
-conn = http.client.HTTPConnection(socket.gethostname(),4573)
 
-# data = {
-# 	"Identificador": "json2",
-# 	"FechaEjecucion": "2016-01-03 06:35:45",
-# 	"Umbral": -80,
-# 	"IdentificadorInstanciaAplicacion": "TESData 2",
-# 	"MuestrasPorTrazo": 1,
-# 	"Duracion": "00:03:00",
-# 	"NumeroTrazos": 10,
-# 	"DuracionPorBanda": 10000,
-# 	"StartFreq": 90500000,
-# 	"StopFreq": 106500000,
-# 	"RefLevel": 5,
-# 	"SweepTime": 0.0001,
-# 	"ResBWidth": 14000,
-# 	"VideoBWidth": 15000,
-# 	"PowerAtt": 10,
-# 	"ScalePDiv": 10,
-# 	"Span": 16000000,
-# }
-#
-# headers = {'Content-type': 'application/json'}
-#
-# mea = {'measurement': 'Hello world github/linguist#1 **cool**, and #1!'}
-# json_mea = json.dumps(data)
-#
-# conn.request("GET", '/update', json.dumps(data), headers)
-# r1 = conn.getresponse()
-# print(r1.status, r1.reason)
-#
-# conn.close()
-# import json
-# import urllib.request
-# req = urllib.request.urlopen('http://localhost:4573/index')
-# req.add_header('Content-Type', 'application/json')
-#
-# response = urllib.request.Request(req, json.dumps(data))
+
 
 import requests
-req = requests.post('http://'+socket.gethostname()+':9000/json_in', json={'filter': 'calif'})
+
+req = requests.post('http://127.0.0.1:8080/measurement', json={"measurement" :
+                                                                              {"id" : " 01",
+ "initial_date" : "2013-02-21, 13:10:20",
+ "final_date": "2013-02-22, 13:10:20 ",
+ "start_freq": 97.5,
+ "final_freq": 105.0,
+ "description": " ",
+ "number_samples": 5,
+ "type" : " loc "
+}
+})
+print(req.json())
--- ServerHttpJson.py
+++ ServerHttpJson.py
@@ -11,27 +11,26 @@ class Servidor(object):
     @cp.tools.json_in()
     @cp.tools.json_out()
     def json_in(self):
-        print(cp.request.json['filter'])
+        print(cp.request.json['measurement'])
         return {}
+
+    @cp.expose
+    @cp.tools.json_in()
+    @cp.tools.json_out(content_type='application/json')
+    def measurement(self):
+        print(cp.request.json["measurement"])
+        js = json.loads(str(cp.request.json["measurement"]).replace("'",'"'))
+        print(js["initial_date"])
+        return {'key': 'value'}
 
     @cp.expose
     def index(self):
         return "<html><body>Hola Mundo</body></html>!"
 
-    @cp.expose
-    def update(self):
-        print("entro")
-        cl = cp.request.headers['Content-Length']
-        #rawbody = cherrypy.request.body.read(int(cl))
-        rawData = cp.request.body.read(int(cp.request.headers['Content-Length']))
-        b = json.load(rawData)
-        #body = json.loads(str(rawbody))
-        print(b)
-        return "Updated r%." % (b,)
+    #def Metodo(self):
+        #Ejecutar metodo
 
-
-    cp.config.update({'server.socket_host':'0.0.0.0','server.socket_port':9000})
 
 if __name__=='__main__':
-    cp.config.update({'server.socket_host':'0.0.0.0','server.socket_port':9000})
+    #cp.config.update({'server.socket_host':'0.0.0.0','server.socket_port':73 99})
     cp.quickstart(Servidor())
