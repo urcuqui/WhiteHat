1a2ad3d63a28eb8a3b641951c8178e8386387d94
Christian Urcuqui <ulcamilo@gmail.com>
2017-09-04 15:38:11 -0500

    python folder
    python folder

--- Python/ClientHttpJson.py
+++ Python/ClientHttpJson.py
@@ -6,7 +6,7 @@ import socket
 
 import requests
 
-req = requests.post('http://localhost:8000/devices/measurement_simon/results/', json={"measurement" :
+req = requests.post('', json={"measurement" :
                                                                               {"id" : " 01",
  "initial_date" : "2013-02-21, 13:10:20",
  "final_date": "2013-02-22, 13:10:20 ",
@@ -14,6 +14,7 @@ req = requests.post('http://localhost:8000/devices/measurement_simon/results/', 
  "final_freq": 105.0,
  "description": " ",
  "number_samples": 5,
- "type" : " otra prueba desde icesi, sera que esta enviando datos raros? "
-},"device":{"identifier" : "prueba"}
+ "type" : " loc "
+}
 })
+print(req.json())
