e6c7639f37e25880cf74682c6f8cfc0b8e6a5ca3
urcuqui <ulcamilo@gmail.com>
2018-09-02 01:17:57 -0500

    brute force to discover passwords with dictionary and hashes file

--- Python - Scripts/3+/Introduction to Python/crypt_basic.py
+++ Python - Scripts/3+/Introduction to Python/crypt_basic.py
@@ -1,7 +1,15 @@
 # Python 3
 # It is only useful in UNIX systems
 # Author: Christian Urcuqui
-# Book reference>
+# Book reference: Stoll, C., & Connolly, J. W. (1990). The cuckoo's egg: Tracking a spy through the maze of computer
+# espionage. Physics Today, 43, 75.
 import crypt
 
+help(crypt)
+# we will try hashing a password using the crypt() function, to do this we will
+# pass the password 'egg' and the salt "HX"
 print(crypt.crypt('egg', 'HX'))
+
+# this is the result of the previous function HX9LLTdc/jiDE
+
+
