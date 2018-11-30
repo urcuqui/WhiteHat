dd41ab98def3ec8c67efe056b80848b5e28f1f13
Christian Urcuqui <rhapsody_cris@hotmail.com>
2018-11-30 00:36:24 -0500

    exam III
    gift

--- My courses/Networks/code/InetAddress/src/web/ClientHandlerHack1.java
+++ My courses/Networks/code/InetAddress/src/web/ClientHandlerHack1.java
@@ -16,40 +16,92 @@ public class ClientHandlerHack1 implements Runnable{
 	public void run() {
 		// TODO Auto-generated method stub
 		System.out.println("\nClientHandler Started for " + this.socket);
-		handleRequest(this.socket);
-		System.out.println("ClientHanlder Terminated for "+  this.socket + "\n");
+		while(true) 
+		{
+			handleRequest(this.socket);
+		}		
+		//System.out.println("ClientHandler Terminated for "+  this.socket + "\n");
 	}
 	
 	public void handleRequest(Socket socket)
 	{
 		try {
+			
+			
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			String headerLine = in.readLine();
-			// A tokenizer is a process that splits text into a series of tokens
-			StringTokenizer tokenizer =  new StringTokenizer(headerLine);
-			//The nextToken method will return the next available token
-			String httpMethod = tokenizer.nextToken();
-			// The next code sequence handles the GET method. A message is displayed on the
-			// server side to indicate that a GET method is being processed
-			if(httpMethod.equals("GET"))
+			if(headerLine!=null)
 			{
-				System.out.println("Get method processed");
-				String httpQueryString = tokenizer.nextToken();
-				StringBuilder responseBuffer =  new StringBuilder();
-				BufferedReader buf = new BufferedReader(new FileReader(System.getProperty("user.dir") +"/src/web/templates/javascr.html"));
-				String str="";
-				while ((str = buf.readLine()) != null) {
-					responseBuffer.append(str);
-			    }
-			    buf.close();
-				sendResponse(socket, 200, responseBuffer.toString());				
-			}
-			else
-			{
-				System.out.println("The HTTP method is not recognized");
-				sendResponse(socket, 405, "Method Not Allowed");
-			}
+				
 			
+				System.out.println(headerLine);
+				// A tokenizer is a process that splits text into a series of tokens
+				StringTokenizer tokenizer =  new StringTokenizer(headerLine);
+				//The nextToken method will return the next available token
+				String httpMethod = tokenizer.nextToken();
+				// The next code sequence handles the GET method. A message is displayed on the
+				// server side to indicate that a GET method is being processed
+				if(httpMethod.equals("GET"))
+				{
+					System.out.println("Get method processed");
+					String httpQueryString = tokenizer.nextToken();
+					System.out.println(httpQueryString);
+					if(httpQueryString.equals("/"))
+					{
+						StringBuilder responseBuffer =  new StringBuilder();
+						String str="";
+						BufferedReader buf = new BufferedReader(new FileReader(System.getProperty("user.dir") +"/src/web/templates/javascr.html"));
+						
+						while ((str = buf.readLine()) != null) {
+							responseBuffer.append(str);
+					    }
+						sendResponse(socket, 200, responseBuffer.toString());		
+					    buf.close();
+					}
+					if(httpQueryString.contains("/?gift="))
+					{
+						System.out.println("Get method processed");
+						String[] response =  httpQueryString.split("gift=");
+						StringBuilder responseBuffer =  new StringBuilder();
+						responseBuffer
+						.append("<html>")
+						.append("<head> <title> hacked! </title> "
+								+ "<script> "
+								+ "window.setTimeout(kuky, 1000);"
+								+ "function kuky(){"
+								+ "while(true){"
+								+ "alert(\"coffee!!\");"
+								+ "};"
+								+ "};"								
+								+ "</script> "
+								+ "<style>"
+								+ "body{"								
+								+ "cursor: url(\"http://www.banderas-del-mundo.com/America_del_Sur/Colombia/colombia_mwd.gif\"), auto;"
+								+ "}"
+								+ "</style>"
+								+ "</head> ")
+						.append("<body bgcolor='black'>")
+						.append("<font color='white'>[0][1][0]</font><br>")
+						.append("<font color='white'>[0][0][1]</font><br>")
+						.append("<font color='white'>[1][1][1]</font><br>")
+						.append("<h1 style='color:white'> NO!!!" + response[1] +"</h1>")
+						.append("<img src='https://s2.glbimg.com/QJD0YP7szRqJuSEUdGHPF_2Dwqs=/850x446/s.glbimg.com/po/tt/f/original/2012/06/01/pirata-e1314380534977.jpg'>")
+						.append("<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/mK4t8U3eSAI?autoplay=1\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" ></iframe>")
+						.append("</body>")
+						.append("</html>");
+						sendResponse(socket, 200, responseBuffer.toString());		
+					    
+					}
+										    
+				}
+				
+				else
+				{
+					System.out.println("The HTTP method is not recognized");
+					sendResponse(socket, 405, "Method Not Allowed");
+				}
+			
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -88,11 +140,14 @@ public class ClientHandlerHack1 implements Runnable{
 				out.writeBytes(statusLine);
 				out.writeBytes("\r\n");
 			}
-			out.close();
+			//out.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 	}
+	
+	
+	
 }
