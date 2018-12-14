962359128797901d634061273925c90cc91fc212
Christian Urcuqui <rhapsody_cris@hotmail.com>
2018-12-14 12:34:58 -0500

    updated

--- Python - Scripts/3+/Network_Basics/network_analysis.py
+++ Python - Scripts/3+/Network_Basics/network_analysis.py
@@ -5,15 +5,15 @@ Date: 14 december 2018
 
 # libraries
 import netifaces
-
+from scapy.all import *
 
 def main():
     nic_available()
 
 
 def nic_available():
-    netifaces.interfaces()
-
+    for interface in get_windows_if_list():
+        print(interface)
 
 if __name__ == "__main__":
     main()
