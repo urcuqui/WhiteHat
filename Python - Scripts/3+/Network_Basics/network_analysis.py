28caed0d483dc07c3fa08c0b7d246e85ebcc65de
Christian Urcuqui <rhapsody_cris@hotmail.com>
2018-12-14 13:34:44 -0500

    updated

--- Python - Scripts/3+/Network_Basics/network_analysis.py
+++ Python - Scripts/3+/Network_Basics/network_analysis.py
@@ -10,6 +10,7 @@ Python version: 3+
 import netifaces
 from scapy.all import *
 import os
+import pyshark
 
 # global variables
 windows = False
@@ -25,16 +26,26 @@ def settings():
 
 def main():
     settings()
-    nic_available()
+    nic_treatment()
 
 
-def nic_available():
+def nic_treatment():
     global windows
     if windows:
         IFACES.show(True)
     else:
         for e in netifaces.interfaces():
             print(e)
+    print(" -------- ")
+    text = input("Write the name of the network interface to use:\n")
+    sniffer(text)
+
+
+def sniffer(type_os):
+    capture = pyshark.LiveCapture(interface='eth0')
+    capture.sniff(timeout=50)
+    capture
+
 
 
 if __name__ == "__main__":
