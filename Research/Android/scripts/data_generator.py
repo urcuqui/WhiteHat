022e58783f6f326bda7d6c58a678784eb56af802
urcuqui <ulcamilo@gmail.com>
2019-01-07 23:47:11 -0500

    updated

--- Research/Android/scripts/data_generator.py
+++ Research/Android/scripts/data_generator.py
@@ -4,7 +4,7 @@ import os
 from os import listdir
 from os.path import isfile
 import optparse
-
+from static_analysis import Manifest
 
 
 """ Author:  Christian Urcuqui
@@ -12,24 +12,6 @@ import optparse
     Description: This project makes a dataset with features from static analysis and 
     dynamic analysis. It is neccesary to use this tool with root privileges    
 """
-
-
-
-apktool_dir = "/usr/local/bin/apktool.bat"
-"""apktool_dir = "sudo apktool"""
-dir_apk = "cd .."
-package_output = "output"
-
-
-def menu():
-    # This is the command menu
-    log = " ___   ___    __   ____  ___    ___    __    __               ".join("\n")
-      .join("|   | /  |   |  |  |  |  |  |  /  |   |  |  |  |      [0][1][0]   ").join("\n")
-#    +   "|   |/  /    |  |  |  |  |  | /  /    |  |  |  |         [0][0][1]    " + "\n"
-#"|      /     |  |  |  |  |      |      \  \_/  /         [1][1][1]    \n" +
-#"|  |\  \     |  '--'  |  |  |\  \        \   /                        \n" +
-#"| _| `.__\   |________|  | _| `.__\      |___|                        \n"
-
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # $$$$$$$$$$$$$$$$$_$$$$$$$$$$$$$$$$_$$$$$$$$$$$$$$$
@@ -63,10 +45,23 @@ def menu():
 
 
 
+apktool_dir = "/usr/local/bin/apktool.bat"
+"""apktool_dir = "sudo apktool"""
+dir_apk = "cd .."
+package_output = "output"
 
-    print(log)
 
-
+def menu():
+    # This is the command menu
+    s = "\n"
+    log = (" ___   ___    __   ____  ___    ___    __    __               ",
+           "|   | /  |   |  |  |  |  |  |  /  |   |  |  |  |      [0][1][0]   ",
+           "|       /    |  |  |  |  |       /    \   \_/  /      [1][1][1]   ",
+           "|      /     |  |  |  |  |      /      \  \_/ /       [1][1][1]   ",
+           "|  |\  \     |  '--'  |  |  |\  \       \    /        Android     ",
+           "| _| `.__\   |________|  | _| `.__\      |___|                    ")
+    out = s.join(log)
+    print(out)
     parser = optparse.OptionParser("Make dataset -d <dictionary> -a <analysis>")
     parser.add_option("-d", dest="dname", type="string", help="APK dictionary")
     parser.add_option("-a", dest="aname", type="string", help="type of analysis")
@@ -77,25 +72,11 @@ def menu():
     else:
         aname = options.aname
         dname = options.dname
+        print(dname)
+        if aname == "static":
+            m = Manifest()
+            m.start(dname)
 
-
-
-class AndroidRE:
-
-    #Use the ApkTool over a list of Apks
-    def ApkTooldefragmenter(self):
-
-        fileList = os.listdir("Samples")
-
-        for filename in fileList:
-            os.system(apktool_dir + " " + "d -f "+"/root/Desktop/Cookie/Samples/"+filename+" -o" + "Output/"+filename)
-            self.eraser(filename)
-    #Erase all files except a androidmanifest.xml
-    def eraser(self, package):
-      os.system("find " +"Output/"+package+ " -type f ! -name 'AndroidManifest.xml' -delete")
-
-#c = AndroidRE()
-#c.ApkTooldefragmenter()
 
 
 if __name__ == "__main__":
