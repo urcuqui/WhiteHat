c34f54e48ce89fe1d76d8ed49e0a483a558db1cb
Christian Urcuqui <rhapsody_cris@hotmail.com>
2020-09-09 23:00:09 -0500

    clase 4

---
+++ My courses/Club Hack1ng/footprinting & reconnaissance/hackbox.py
@@ -0,0 +1,44 @@
+""" Author:  Christian Urcuqui
+    Last update: 9 September 2020
+    Description: This script is only to teach about python and some ethical hacking analysis in order to the students
+    learn about the process and join to the hack the box challenges
+"""
+
+import requests
+import json
+
+s = "\n"
+log = (" ___   ___    __   ____  ___    ___    __    __               ",
+       "|   | /  |   |  |  |  |  |  |  /  |   |  |  |  |      [0][1][0]   ",
+       "|       /    |  |  |  |  |       /    \   \_/  /      [0][0][1]   ",
+       "|      /     |  |  |  |  |      /      \  \_/ /       [1][1][1]   ",
+       "|  |\  \     |  '--'  |  |  |\  \       \    /             ",
+       "| _| `.__\   |________|  | _| `.__\      |___|                    ")
+out = s.join(log)
+print(out)
+print()
+
+
+headers = {"User-Agent": "Gecko"}
+
+data = json.loads(requests.post("https://www.hackthebox.eu/api/invite/how/to/generate", headers=headers).content.decode())
+print(data)
+print(data["data"]["data"])
+print(data["data"]["enctype"])
+
+if data["data"]["enctype"] == "BASE64":
+    import base64
+    base64_message = data["data"]["data"]
+    base64_bytes = base64_message.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    message = message_bytes.decode('ascii')
+    print(message)
+
+if data["data"]["enctype"] == "ROT13":
+
+    rot13trans = str.maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+       'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm')
+    print(data["data"]["data"].translate(rot13trans))
+
+
+#print(requests.post("https://www.hackthebox.eu/api/invite/generate", headers=headers).content)
